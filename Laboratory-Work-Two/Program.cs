900516d612c0a3fa493899825c3698e6ed3ecabc
kuraev99daniil <kuraev.dm99@gmail.com>
2021-10-21 01:03:44 +0300

    Init commit

---
+++ Laboratory-Work-Two/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratory_Work_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
